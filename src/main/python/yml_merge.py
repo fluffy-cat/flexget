fe87db67875bc9657df7f730560209bd42cce425
Daniel <daniel@daniel.com>
2019-06-04 22:35:41 +0800

    Added default config

--- src/main/python/yml_merge.py
+++ src/main/python/yml_merge.py
@@ -2,7 +2,7 @@ import sys
 import glob
 import hiyapyco
 
-configs = glob.glob(sys.argv[1])
-configs.sort()
-conf = hiyapyco.load(configs, method=hiyapyco.METHOD_MERGE)
+paths = [sorted(glob.glob(path)) for path in sys.argv[1:]]
+configs = [conf for sublist in paths for conf in sublist]
+conf = hiyapyco.load(configs, method=hiyapyco.METHOD_MERGE, mergelists=False)
 print(hiyapyco.dump(conf))
